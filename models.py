e6c977e3e322336782df2cf69dddef97e43d7974
shubhankar71 <mshubhanakarrao@gmail.com>
2026-07-19 15:56:28 +0530

    update on the login and sign up

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
 
 
